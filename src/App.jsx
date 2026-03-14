d75d16f3484d7682119c2a5bf7bcff8f33e494b9
AravinthanRamasamy <aravinthan.ramasamy@aspiresys.com>
2026-03-14 09:52:00 +0000

    Update siteProps with personal information and add hover animation to social icons

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Aravinthan Ramasamy",
+  title: "Test Engineer",
+  email: "aravinthan.ramasamy@aspiresys.com",
+  gitHub: "aravinthan-ramasamy",
+  instagram: "aravinthan_ramasamy",
+  linkedIn: "aravinthan-ramasamy",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "aravinthan_ramasamy",
+  youTube: "aravinthan-ramasamy",
 };
 
 const primaryColor = "#4E567E";
